f26b3267f3968442d49400534742165169099018
Terry Sun <terrynsun@gmail.com>
2019-04-29 22:24:46 -0700

    Better init script

--- init.sh
+++ init.sh
@@ -1,14 +1,36 @@
 #!/bin/bash
 
 # Oh My Zsh (copy of online install script)
-git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+if [ ! -d ~/.oh-my-zsh ]; then
+    git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+else
+    echo oh-my-zsh already installed
+fi
 
 # Vim Undodir
-mkdir ~/.vim/undo
+mkdir -p ~/.vim/undo
 
 # Vundle
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-vim +PluginInstall +qall
+if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
+    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+    vim +PluginInstall +qall
+else
+    echo vundle already installed
+fi
 
 # TPM
-git clone https://github.com/tmux-plugins/tpm /home/sun/.tmux/plugins/tpm
+if [ ! -d ~/.tmux/plugins/tpm ]; then
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+else
+    echo tpm already installed
+fi
+
+# Symlink all dotfiles
+cd home
+
+for f in $(ls -A); do
+    # if file does not exist
+    if [ ! -f ~/$f ] && [ ! -d ~/$f ]; then
+        ln -s $(pwd)/$f ~/$f
+    fi
+done
